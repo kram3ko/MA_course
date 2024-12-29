ffea14ec69cc454f98047a7339905d2b0a848f5c
Vinohradov Volodymyr <eXpertt13@gmail.com>
2024-12-29 18:05:07 +0200

    Initial Commit

---
+++ 1.py
@@ -0,0 +1,15 @@
+def message_people(people: list) -> callable:
+    def print_message(message: str) -> None:
+        if message == "hello":
+            print(f"Hello, {', '.join(people)}, nice to see you!")
+        elif message == "meeting":
+            print(f"{', '.join(people)}, we have a meeting in an hour, don't be late!")
+        elif message == "bye":
+            print(f"Bye {', '.join(people)}, see you tomorrow!")
+
+    return print_message
+
+
+mes_people = message_people(["Alex", "Robert", "Tom"])
+mes_people("hello")
+mes_people("bye")
